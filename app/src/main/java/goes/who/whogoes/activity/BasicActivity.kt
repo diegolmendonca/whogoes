c6127337a8d1349ff255f561e63bdb665cf25a32
Mendonca <doma@gft.com>
2017-09-27 01:19:30 +0200

    improving app bar and adding logout

---
+++ app/src/main/java/goes/who/whogoes/activity/BasicActivity.kt
@@ -0,0 +1,12 @@
+package goes.who.whogoes.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class BasicActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_basic)
+    }
+}
