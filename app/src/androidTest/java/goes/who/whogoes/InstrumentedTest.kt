254416a56218807cddfe654cc63d81d7e8ab8045
Natan Facchin <maxmousee@gmail.com>
2017-10-05 14:10:35 +0200

    CircleCI AVD settings adjustments

--- app/src/androidTest/java/goes/who/whogoes/InstrumentedTest.kt
+++ app/src/androidTest/java/goes/who/whogoes/InstrumentedTest.kt
@@ -1,13 +1,20 @@
 package goes.who.whogoes
 
 import android.support.test.InstrumentationRegistry
+import android.support.test.espresso.Espresso
+import android.support.test.espresso.assertion.ViewAssertions
+import android.support.test.espresso.matcher.ViewMatchers
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
-import goes.who.whogoes.activity.MainActivity
 import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import android.view.WindowManager
+import goes.who.whogoes.activity.MainActivity
+import org.junit.Before
+
+
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -19,18 +26,27 @@ class InstrumentedTest {
     @get:Rule
     val mActivityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
 
+    @Before
+    fun setUp() {
+        val activity = mActivityRule.activity
+        val wakeUpDevice = Runnable {
+            activity.window.addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON or
+                    WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED or
+                    WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON)
+        }
+        activity.runOnUiThread(wakeUpDevice)
+    }
+
     @Test
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getTargetContext()
         assertEquals("goes.who.whogoes", appContext.packageName)
     }
-
-    /*
+    
     @Test
     fun mainActivityIsDisplayedOnView() {
         Espresso.onView(ViewMatchers.withId(R.id.main_activity_rel_layout))
                 .check(ViewAssertions.matches(ViewMatchers.isDisplayed()));
     }
-    */
 }
